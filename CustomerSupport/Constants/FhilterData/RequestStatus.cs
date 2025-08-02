7ea54a7364ca3bb4933f2fceb6cc20432b7d3853
vampire7432804732985734985743948 <romannagik@gmail.com>
2025-08-02 03:30:26 -1100

    .

---
+++ CustomerSupport/Constants/FhilterData/RequestStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace CustomerSupport.Constants.FhilterData
+{
+    public class RequestStatus
+    {
+        public const string All = "All";
+        public const string Open = "Open";
+        public const string InProgress = "InProgress";
+        public const string WaitingForUser = "WaitingForUser";
+        public const string OnHold = "OnHold";
+        public const string Resolved = "Resolved";
+        public const string Closed = "Closed";
+        public const string Reopened = "Reopened";
+        public const string Canceled = "Canceled";
+    }
+}
---
+++ CustomerSupport/Constants/FhilterData/SubmittionDate.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomerSupport.Constants.FhilterData
+{
+    public class SubmittionDate
+    {
+        public const string Recent = "Recent";
+        public const string Oldest = "Oldest";
+        public const string TheMostPopular = "The most popular";
+    }
+}
